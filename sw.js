823608f48d1a4b489c54cfee4966f9c707a78dbd
Lectrom447 <EmanuelMendezemanuel-22@hotmail.com>
2021-08-29 00:06:22 -0600

    Optimizaciones para IOS

--- sw.js
+++ sw.js
@@ -11,9 +11,9 @@ var swUtlLocation = 'js/sw-utils.js'
 
 importScripts(swUtlLocation)
 
-const STATIC_CACHE = 'static-v3'
-const DYNAMIC_CACHE = 'dynamic-v1'
-const INMUTABLE_CACHE = 'inmutable-v1'
+const STATIC_CACHE = 'static-v4'
+const DYNAMIC_CACHE = 'dynamic-v2'
+const INMUTABLE_CACHE = 'inmutable-v2'
 
 const APP_SHELL = [
     'index.html',
